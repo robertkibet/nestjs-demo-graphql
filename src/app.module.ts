1891a0c953102b1e7b4b6e1ce0d8e00fb0f30b5c
WORKSTATION\workstation <kipronokrobert@gmail.com>
2021-09-16 19:02:54 +0300

    add sqlite database and basic config with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path/posix';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,12 @@ import { PetsModule } from './pets/pets.module';
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // using sqlite database
+      database: ':memory:', // run database in memory, any restart is a fresh database
+      entities: ['dist/**/*.entity{.ts,.js}'], // searches for entities within dist
+      synchronize: true, // auto sync and update schema to match database, always use migrations for production level code
     }),
     PetsModule,
   ],
